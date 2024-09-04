e4398162e2f5cf4e049c39a4c258a30c066755b1
Deek <deek9517@gmail.com>
2024-09-05 00:19:57 +0300

    new methods

--- SLL/front.py
+++ SLL/front.py
@@ -30,11 +30,68 @@ class SLL:
             print("Null")
         else:
             print(self.head.data)
+    def contains(self ,value):
+        runner = self.head
+        while(runner != None):
+            if runner.data == value:
+                print("True")
+                return self
+            runner = runner.next
+        print("False")
+        return self
+    def length(self):
+        runner = self.head
+        count = 0
+        while(runner != None):
+            count +=1
+            runner = runner.next
+        print(count)
+    def display(self):
+        runner = self.head
+        list = []
+        while(runner != None):
+            list.append(runner.data)
+            runner = runner.next
+        print(list)
+    def max(self):
+        runner = self.head
+        val = self.head.data
+        while(runner.next != None):
+            if runner.data > val:
+                val = runner.data 
+            runner = runner.next
+        print(val)
+    def min(self):
+        runner = self.head
+        val = self.head.data
+        while(runner.next != None):
+            if runner.data < val:
+                val = runner.data 
+            runner = runner.next
+        print(val)
+    def avg(self):
+        runner = self.head
+        val = 0
+        count = 0
+        while(runner.next != None):
+            val += runner.data
+            count += 1
+            runner = runner.next
+        print(val/count)
 
 
 SLL1 = SLL()
-SLL1.addFront(18).addFront(5)
-SLL1.addFront(73)
+SLL1.addFront(18).addFront(5).addFront(73)
 SLL1.remove_front()
 SLL1.traverse()
 SLL1.front()
+SLL1.contains(5)
+SLL1.contains(18)
+SLL1.contains(73)
+SLL1.length()
+SLL1.addFront(6).addFront(52)
+SLL1.length()
+SLL1.display()
+SLL1.max()
+SLL1.min()
+SLL1.avg()
