f0c86fbeac2e5ea0b4e07d8293e24ef992c7f80e
Deek <deek9517@gmail.com>
2024-09-05 14:22:08 +0300

    recrusion

---
+++ SLL/recrusion.py
@@ -0,0 +1,24 @@
+
+import math
+
+def rSigma(num):
+    if num <= 0:
+        return 0
+    intNum = math.trunc(num)
+    prevVal = rSigma(intNum - 1)
+    print(prevVal + intNum)
+    return prevVal + intNum
+rSigma(-1)
+rSigma(5)
+
+
+def rFact(num):
+    if num <= 0:
+        return 1
+    intNum = math.trunc(num)
+    prevVal =intNum* rFact(intNum - 1)
+    print(prevVal)
+    return prevVal 
+
+rFact(3)
+rFact(6.5)
