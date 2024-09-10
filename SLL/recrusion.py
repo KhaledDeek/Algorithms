df387118800d1e5383ffb59b655990c29b109e63
Deek <deek9517@gmail.com>
2024-09-10 15:44:00 +0300

    recrusion

--- SLL/recrusion.py
+++ SLL/recrusion.py
@@ -24,3 +24,34 @@ rFact(3)
 rFact(6.5)
 
 
+def rBinarySearch(arr,val):
+    if len(arr) == 0:
+        print(False)
+        return False
+    if arr[0] == val:
+        print(True)
+        return True
+    return rBinarySearch(arr[1:], val)
+
+
+
+rBinarySearch([1,3,5,6],4)
+
+
+
+def rGCF(num1,num2):
+    if num2 == num1 :
+        return num1
+    if num2 > num1 and num2%num1 == 0:
+        print(num1)
+        return num1
+    if num2 < num1 and num1%num2 == 0:
+        print(num2)
+        return num2
+    if num1 > num2 :
+        return rGCF(num1-num2,num2)
+    if num2 > num1 : 
+        return rGCF(num1 , num2-num1)
+
+rGCF(9 , 3)
+rGCF(12 , 22)
